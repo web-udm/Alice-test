46f5685fdfd7dc6b5a9613c96525431a069ba126
moildar <web.udm@gmail.com>
2021-01-09 19:32:18 +0400

    Добавил приветственную фразу

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Alice, Reply } = require('yandex-dialogs-sdk');
 const alice = new Alice();
 
-alice.any(async ctx => Reply.text('Привет, я мяви-какави'));
+alice.any( async ctx => {
+    return Reply.text('Это игра "Угадай цитату из фильма". Все просто: я произношу фразу, а вы называете фильм, ' +
+        'в котором она звучала. Начнем?');
+});
 
 const server = alice.listen(3001, '/');
